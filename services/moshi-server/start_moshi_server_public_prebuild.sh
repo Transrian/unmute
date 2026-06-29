292a0d339792338e13f362d109a8b8727e5bc678
Valentin Bourdier <valentin.bourdier@mail.com>
2026-06-29 21:33:50 +0200

    no more tokens required

--- services/moshi-server/start_moshi_server_public_prebuild.sh
+++ services/moshi-server/start_moshi_server_public_prebuild.sh
@@ -4,8 +4,6 @@ set -ex
 
 export LD_LIBRARY_PATH=$(python3.12 -c 'import sysconfig; print(sysconfig.get_config_var("LIBDIR"))')
 
-uvx hf auth login --token $HUGGING_FACE_HUB_TOKEN
-
 # Subtle detail here: We use the full path to `moshi-server` because there is a `moshi-server` binary
 # from the `moshi` Python package. We'll fix this conflict soon.
 /root/.cargo/bin/moshi-server $@
